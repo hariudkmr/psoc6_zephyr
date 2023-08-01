bb31e2466164a2d2ebd2a3caf6f2699d285672c8
INFINEON\udayakumar <hari.udayakumar@infineon.com>
2023-08-01 23:05:06 +0530

    compile and debuggd button example cod in vscode

---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,13 @@
+empty_file.o: C:/Users/udayakumar/zephyrproject/zephyr/misc/empty_file.c \
+ C:/Users/udayakumar/zephyrproject/zephyr/boards/arm/cy8cproto_062_4343w/cy8cproto_062_4343w.dts \
+ C:/Users/udayakumar/zephyrproject/zephyr/dts/arm/infineon/psoc6/mpns/CY8C624ABZI_S2D44.dtsi \
+ C:/Users/udayakumar/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
+ C:/Users/udayakumar/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ c:\users\udayakumar\zephyrproject\zephyr\dts\arm\infineon\psoc6\psoc6_02\psoc6_02.124-bga.dtsi \
+ C:/Users/udayakumar/zephyrproject/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ C:/Users/udayakumar/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/ifx_cat1-pinctrl.h \
+ c:\users\udayakumar\zephyrproject\zephyr\dts\arm\infineon\psoc6\psoc6_02\psoc6_02.dtsi \
+ C:/Users/udayakumar/zephyrproject/zephyr/dts/common/mem.h \
+ C:/Users/udayakumar/zephyrproject/zephyr/dts/arm/infineon/psoc6/system_clocks.dtsi \
+ C:/Users/udayakumar/zephyrproject/zephyr/boards/arm/cy8cproto_062_4343w/cy8cproto_062_4343w-common.dtsi \
+ C:/Users/udayakumar/zephyrproject/zephyr/boards/arm/cy8cproto_062_4343w/cy8cproto_062_4343w-pinctrl.dtsi
